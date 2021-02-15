105dd96bfe00bf7979cde6f3d59142d774a2aefa
Sebastian Pichler <s.pichler100795@gmail.com>
2021-02-15 10:47:20 +0100

    add exercise

--- src/src/main.java
+++ src/src/main.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Gib eine Uhrzeit ein im Format 0-24");
-        int zeit= scanner.nextInt();
+        int zeit = scanner.nextInt();
 
-        if(zeit==12){
+        if (zeit == 12) {
             System.out.println("Mittagspause");
-        }else if (zeit<=7){
+        } else if (zeit <= 7) {
             System.out.println("Freizeit");
-        }else if ((zeit<=11)  || (zeit<=16)){
+        } else if (zeit <= 16) {
             System.out.println("Arbeitszeit");
-        }else{
+        } else {
             System.out.println("Freizeit");
         }
     }
